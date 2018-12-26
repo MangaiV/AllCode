d55ae9953dbb3f76d245e077a6cbc829e941a653
MangaiV <46150004+MangaiV@users.noreply.github.com>
2018-12-26 12:29:19 +0530

    Add files via upload

---
+++ Hello_World.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author mvelusamy
+ *
+ */
+public class Hello_World {
+
+	/**
+	 * 
+	 */
+	public Hello_World() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
